399bc6a4cd59d5611d96077a90a72220adbd0b46
Agus Navazzotti <marianavazzotti@gmail.com>
2022-04-03 20:07:00 -0300

    solucionar comparaciones en ccondicional

--- JSClase3/index.js
+++ JSClase3/index.js
@@ -17,54 +17,54 @@ const tarea4 = new Tarea("Lavar los platos", "3");
 
 alert("¡Bienvenido al gestionador de tareas! A continuación te mostraremos diferentes tareas, escribe 1 si las realizaste o 0 si no las hiciste");
 
-let puntos = 0;
+var puntos=0;
 let tar1 = parseInt(prompt("Lavar los pisos"));
 
-if(tar1=1){
+if(tar1==1){
     tarea1.hacerTarea();
     if(tarea1.dificultad > 5 && tarea1.realizada){
-        puntos= puntos+10
+        puntos= puntos+10;
     }else{
-        puntos= puntos+5
+        puntos= puntos+5;
     }
 }
 
 let tar2 = parseInt(prompt("Limpiar el baño"));
 
-if(tar2=2){
+if(tar2==1){
     tarea2.hacerTarea();
-    if(tarea2.dificultad > 5 && tarea2.realizada){
-        puntos= puntos+10
+    if(tarea2.dificultad > 5){
+        puntos= puntos+10;
     }else{
-        puntos= puntos+5
+        puntos= puntos+5;
     }
 }
 
 let tar3 = parseInt(prompt("Cortar el pasto"));
 
-if(tar3=1){
+if(tar3==1){
     tarea3.hacerTarea();
-    if(tarea3.dificultad > 5 && tarea3.realizada){
-        puntos= puntos+10
+    if(tarea3.dificultad > 5){
+        puntos= puntos+10;
     }else{
-        puntos= puntos+5
+        puntos= puntos+5;
     }
 }
 
 let tar4 = parseInt(prompt("Lavar los platos"));
 
-if(tar4=1){
+if(tar4==1){
     tarea4.hacerTarea();
     if(tarea4.dificultad > 5 && tarea4.realizada){
-        puntos= puntos+10
+        puntos= puntos+10;
     }else{
-        puntos= puntos+5
+        puntos= puntos+5;
     }
 }
+alert(`${puntos}`);
 
-
-if(puntos>20){
+if(puntos>=20){
     alert("¡Felicitaciones! Hiciste suficiente por hoy, la cena hoy la trae el delivery")
-}else if(puntos<=20){
+}else if(puntos<20){
     alert("Día de flojera. Si no viste los simuladores es un buen día para empezar")
 }
